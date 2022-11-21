3baf87bba7ddf5ed4d7a108467c68666a878fe47
kinopico <zqmillet@qq.com>
2022-11-21 20:37:24 +0800

    update

---
+++ testcases/table_test.py
@@ -0,0 +1,20 @@
+from sphinxcontrib.table.table import Table
+from sphinxcontrib.table.span import Span
+from sphinxcontrib.table.coordinate import Coordinate
+
+def test_table():
+    table = Table(
+        data=[
+            ["Header 1", "Header 2", "Header3", "Header 4"],
+            ["row 1", "column 2", "column 3", "column 4 苟利"],
+            ["row 2", "Cells span columns. and something else, hahahahahahahahahahahahah", "", ""],
+            ["row 3", ".. hint::\n\n    def add(x, y):\n        return x + y", "- Cells\n- contain\n- blocks", ""],
+            ["row 4", "", "", ""]
+        ],
+        spans=[
+            Span(Coordinate(2, 1), rows=1, columns=3),
+            Span(Coordinate(3, 1), rows=2, columns=1),
+            Span(Coordinate(3, 2), rows=2, columns=2)
+        ]
+    )
+    print(table.render())
