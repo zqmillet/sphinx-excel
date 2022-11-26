cede40e5531d2be6f14aa313c45fda4f298afcb2
kinopico <zqmillet@qq.com>
2022-11-26 15:33:44 +0800

    update

---
+++ sphinxcontrib/table/__init__.py
@@ -0,0 +1,34 @@
+"""
+this is the package sphinxcontrib.console.
+"""
+__version__ = (1, 0, 9)
+
+from os.path import join
+from os.path import dirname
+
+from sphinx.util.fileutil import copy_asset
+
+from .bash import setup as setup_bash
+from .python import setup as setup_python
+
+def copy_asset_files(application, execution):
+    """
+    this function is used to copy css file to build directory.
+    """
+    asset_files = [join(dirname(__file__), 'sphinx_console.css')]
+    if execution is None: # build succeede
+        for path in asset_files:
+            copy_asset(path, join(application.outdir, '_static'))
+
+def setup(application):
+    """
+    setup extension.
+    """
+    setup_bash(application)
+    setup_python(application)
+
+    application.add_config_value('sphinx_console_cache_dir', None, '')
+    application.add_css_file('sphinx_console.css')
+    application.connect('build-finished', copy_asset_files)
+
+    return {"version": __version__, "parallel_read_safe": True}
